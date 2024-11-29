a354d25615558b70abf73ef70ad987a0ee5ba599
DomCaio <reicaiodom@gmail.com>
2024-11-28 23:07:21 -0300

    Fix: Correct welcome message

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,5 @@
 print("Hello Wordl")
 
 def mostrar_mensagem():
-fix/corrigir-mensagem
     print("Mensagem atualizada: Bem-vindo ao repositório do projeto de controle de versão!")
-main
 mostrar_mensagem()
