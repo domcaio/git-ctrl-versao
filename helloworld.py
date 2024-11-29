b9eb13b814d0fe67c3ac5e472bdcf59918a30dac
DomCaio <reicaiodom@gmail.com>
2024-11-28 23:16:09 -0300

    Add função para calcular a soma de dois números

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,10 @@ print("Hello Wordl")
 def mostrar_mensagem():
     print("Mensagem atualizada: Bem-vindo ao repositório do projeto de controle de versão!")
 mostrar_mensagem()
+
+def soma(a, b):
+    return a + b
+
+# Exemplo de uso
+resultado = soma(5, 7)
+print(f"O resultado da soma é: {resultado}")
